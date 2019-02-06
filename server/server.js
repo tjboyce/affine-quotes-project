a017d707f96e03b2dc687b13a6d76a5e850ee94d
Todd Boyce Jr <tjboyce@me.com>
2019-02-06 14:05:36 -0600

    initial commit

---
+++ server/server.js
@@ -0,0 +1,21 @@
+console.log('server.js');
+
+let express = require('express');
+
+let app = express();
+
+app.use(express.static('server/public'));
+
+app.get('/quotes', function (req, res) {
+    res.send(quotes_data);
+});
+
+app.listen(5000, function () {
+    console.log('Running on port 5000');
+});
+let quotes_data = [
+    { quote: 'I\'m not going to school just for the academics - I wanted to share ideas, to be around people who are passionate about learning.', author: 'Emma Watson' },
+    { quote: 'Remember there\'s no such thing as a small act of kindness. Every act creates a ripple with no logical end.', author: 'Scott Adams' },
+    { quote: 'Intelligence plus character-that is the goal of true education.', author: 'Martin Luther King, Jr.' }
+];
+
