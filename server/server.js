ea4449da0523191ede70d3b404fd01407bfc78a2
Todd Boyce Jr <tjboyce@me.com>
2019-02-06 15:11:16 -0600

    add quotes route to sever

--- server/server.js
+++ server/server.js
@@ -1,21 +1,26 @@
-console.log('server.js');
-
 let express = require('express');
-
+let PORT = 5000;
 let app = express();
 
-app.use(express.static('server/public'));
+app.listen(PORT, () => {
+    console.log('port running on', PORT);
+});
 
-app.get('/quotes', function (req, res) {
+app.get('/quotes', (req, res) => {
     res.send(quotes_data);
 });
 
-app.listen(5000, function () {
-    console.log('Running on port 5000');
-});
+app.use(express.static('server/public'));
+
+
+
+
 let quotes_data = [
     { quote: 'I\'m not going to school just for the academics - I wanted to share ideas, to be around people who are passionate about learning.', author: 'Emma Watson' },
     { quote: 'Remember there\'s no such thing as a small act of kindness. Every act creates a ripple with no logical end.', author: 'Scott Adams' },
     { quote: 'Intelligence plus character-that is the goal of true education.', author: 'Martin Luther King, Jr.' }
 ];
 
+// CRUD:
+     // Create,   Read/Retrieve,   Update,  Delete
+        //post    //get            //put     //delete
