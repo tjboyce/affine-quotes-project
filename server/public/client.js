0d92749041c31b9e38d59fcf25690639e91c8d78
Todd Boyce Jr <tjboyce@me.com>
2019-02-06 16:51:50 -0600

    append the DOM with AJAX

--- server/public/client.js
+++ server/public/client.js
@@ -4,9 +4,29 @@ $(document).ready(onReady);
 
 function onReady() {
     console.log('jQ has been loaded');
+    
+    $.ajax({
+        url: '/quotes',
+        method: 'GET'
+    }).then(function (response){
+        console.log('The quotes get request worked!!!!');
+        console.log(response);
+        for (let i = 0; i < response.length; i++) {
+           // $('#tbody').append(response[i]);
+            $('#tbody').append(`<br><tr><td>
+                ${response[i].quote}
+                </td>
+                <td>
+                ${response[i].author}
+                </td></tr>`);  
+        }
+        
+    });
+    
     $('#tbody').append(`<br><tr><td>
             "be excellent to eachother. and party on dudes!"
-                </td><td>
+                </td>
+                <td>
            bill and teds excellent adventure.
                 </td></tr>`);
 }
